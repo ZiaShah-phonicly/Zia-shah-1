25eabe9eaede348c5f3f3e644a3f5c9c107344a2
Zia Shah <166240167+ZiaShahZS@users.noreply.github.com>
2024-04-10 19:47:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-app = Flask(__name__,')
+app = Flask(__name__,)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['phonicly_db']
 users_collection = db['users']
